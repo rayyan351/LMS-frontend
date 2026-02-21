62def88d8dffc753cf768c39433e6fd7e040c66f
rayyan351 <rayyanhabib1@gmail.com>
2026-02-22 04:23:23 +0500

    frontend

---
+++ src/api/api.js
@@ -0,0 +1,17 @@
+const API = "https://assignment-tracker-backend-7xiw.onrender.com";
+
+export async function api(path, options = {}) {
+  const token = localStorage.getItem("token");
+
+  const res = await fetch(`${API}${path}`, {
+    headers: {
+      "Content-Type": "application/json",
+      ...(token && { Authorization: token })
+    },
+    ...options
+  });
+
+  const data = await res.json();
+  if (!res.ok) throw data;
+  return data;
+}
