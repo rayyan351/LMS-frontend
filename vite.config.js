8758f54ed5d5bba66b98ddd89704249a63b49da5
rayyan351 <rayyanhabib1@gmail.com>
2026-02-22 05:19:14 +0500

    work

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/",  // important for correct asset paths
-  build: {
-    outDir: "dist" // ensure Vite builds to dist
-  }
-});
+})
