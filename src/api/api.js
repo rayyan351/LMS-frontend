ca125a2dc3ad4a76b159908e4c44ee035f287ab2
rayyan351 <rayyanhabib1@gmail.com>
2026-02-22 05:01:50 +0500

    frontend update

--- src/api/api.js
+++ src/api/api.js
@@ -1,12 +1,13 @@
-const API = "https://assignment-tracker-backend-7xiw.onrender.com";
+const API = "https://assignment-tracker-backend-7xiw.onrender.com/api";
 
 export async function api(path, options = {}) {
   const token = localStorage.getItem("token");
 
   const res = await fetch(`${API}${path}`, {
+    mode: "cors",
     headers: {
       "Content-Type": "application/json",
-      ...(token && { Authorization: token })
+      ...(token && { Authorization: `Bearer ${token}` })
     },
     ...options
   });
