1ddffc02c9552b2c453c8bd66752915e6031e203
rayyan351 <rayyanhabib1@gmail.com>
2026-02-22 05:14:35 +0500

    vercel issue

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/",  // important for correct asset paths
-})
+  build: {
+    outDir: "dist" // ensure Vite builds to dist
+  }
+});
